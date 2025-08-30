eb0b529d9d3882398fb99a20729d8d84ed7db003
Vidya <1980vidyam@gmail.com>
2025-08-30 20:26:28 +0530

    Add files via upload

---
+++ 01_Patterns/Pattern18.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void print(int n){
+    
+    for(int i=0;i<n;i++){
+        for(int j=i;j>=0;j--){
+            cout<<char('E'-j);
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    print(n);
+    return 0;
+}
