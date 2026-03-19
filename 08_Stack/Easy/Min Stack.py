5250eacc5a85f8094a46b4eb81b3255673156c8c
Vidya <1980vidyam@gmail.com>
2026-03-19 18:24:04 +0530

    Create Min Stack.py
    Implement MinStack class with push, pop, top, and getMin methods.

---
+++ 08_Stack/Easy/Min Stack.py
@@ -0,0 +1,23 @@
+class MinStack(object):
+    def __init__(self):
+        self.stack = []
+    def push(self, val):
+        self.stack.append(val)
+    def pop(self):
+        if self.stack:
+            self.stack.pop()
+    def top(self):
+        if self.stack:
+            return self.stack[-1]
+    def getMin(self):
+        if self.stack:
+            return min(self.stack)
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
